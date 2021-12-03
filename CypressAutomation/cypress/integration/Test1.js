2f956d4a79efba905db9147244de209d54f29fe0
ADMIN\Admin <odao@openwt.com>
2021-12-03 16:14:37 +0700

    Update First Testcase

--- CypressAutomation/cypress/integration/Test1.js
+++ CypressAutomation/cypress/integration/Test1.js
@@ -7,7 +7,23 @@ it('Open URL and searching',function()
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
     cy.get('input.search-keyword').type('be')
     cy.wait(2000)
-    cy.get('.products-wrapper .product-name').should('have.length',5)
+    //Find child chaining
+    cy.get('.products').find('.product').should('have.length',5)
+    //Click the add to cart button on the third product
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+    // Click on product with name "abc"
+    cy.get('.products').find('.product').each(($el, index, $list)=>{
+        const Varleg= $el.find('h4.product-name').text()
+        if(Varleg.includes('Beans')){
+            //$el.find('button').click()
+            cy.wrap($el).find('button').click()
+        }
+
+    }
+
+    )
+    
+    
 } )
 
 
